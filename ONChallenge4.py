85549134462cc6ac91475ae711009167630fcd1d
db1985 <49497305+db1985@users.noreply.github.com>
2020-06-04 20:12:55 +0100

    Add files via upload

---
+++ ONChallenge4.py
@@ -0,0 +1,16 @@
+#Pre-requisites
+
+import re
+
+#Allow user to enter a times table (only up to table 12) and a number of rows to display.
+
+number = int(input("Display the multiplication table for:"))
+
+while number > 12:
+    print("Invalid table number. Please use 1-12")
+    number = int(input("Display the multiplication table for:"))
+else:
+    for i in range(0,5):
+        print(number,"x",i,"=",number*i)
+
+
