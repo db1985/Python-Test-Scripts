730c765ad6aeabfaaddc107f4d698b63bbbdb91b
db1985 <49497305+db1985@users.noreply.github.com>
2020-06-05 10:49:00 +0100

    Add files via upload

--- inchtocm.py
+++ inchtocm.py
@@ -1,9 +1,19 @@
 #inch to cm
 
-def inch_cm(number):
+def inch_to_cm(number):
     conversion = number * 2.54
     return conversion
 
 mynum = float(input("Enter a number in inches:"))
-result = inch_cm(mynum)
+result = inch_to_cm(mynum)
 print(f"{mynum} in inches is {result} in cm")
+
+#cm to inch
+
+def cm_to_inch(number):
+    conversion = number / 2.54
+    return conversion
+
+mynum = float(input("Enter a number in cm:"))
+result = cm_to_inch(mynum)
+print(f"{mynum} in cm is {result} in inches")
